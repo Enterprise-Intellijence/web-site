c6a3630a5f3cd25bc4af1e68df87364fcba71954
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-06 22:05:20 +0200

    added ng-bootstrap and working on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DumbTestComponent } from './components/dumb-test/dumb-test.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DumbTestComponent
+    DumbTestComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
