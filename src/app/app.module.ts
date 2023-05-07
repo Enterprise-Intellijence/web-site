d560b9ba82793de753a589d590babca480d78161
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-05-07 10:44:18 +0200

    Added settings folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,24 @@ import { AppComponent } from './app.component';
 import { DumbTestComponent } from './components/dumb-test/dumb-test.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProfileDetailsComponent } from './pages/profile-details/profile-details.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AccountSettingsComponent } from './pages/settings/account-settings/account-settings.component';
+import { ShippingComponent } from './pages/settings/shipping/shipping.component';
+import { PaymentsComponent } from './pages/settings/payments/payments.component';
+import { NotificationsComponent } from './pages/settings/notifications/notifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DumbTestComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProfileDetailsComponent,
+    SettingsComponent,
+    AccountSettingsComponent,
+    ShippingComponent,
+    PaymentsComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
