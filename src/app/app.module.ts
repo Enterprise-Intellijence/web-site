f1a6fc9fe0cea4ed960b163a7ef7555d46e263ca
Emanuele Conforti <manu.conforti@gmail.com>
2023-05-07 13:24:38 +0200

    product components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,15 @@ import { AppComponent } from './app.component';
 import { DumbTestComponent } from './components/dumb-test/dumb-test.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ProfileDetailsComponent } from './pages/profile-details/profile-details.component';
+import { ProfileDetailsComponent } from './pages/settings/profile-details/profile-details.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { AccountSettingsComponent } from './pages/settings/account-settings/account-settings.component';
 import { ShippingComponent } from './pages/settings/shipping/shipping.component';
 import { PaymentsComponent } from './pages/settings/payments/payments.component';
 import { NotificationsComponent } from './pages/settings/notifications/notifications.component';
+import { ProductImagesComponent } from './components/product-images/product-images.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductInfoComponent } from './components/product-info/product-info.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { NotificationsComponent } from './pages/settings/notifications/notificat
     AccountSettingsComponent,
     ShippingComponent,
     PaymentsComponent,
-    NotificationsComponent
+    NotificationsComponent,
+    ProductImagesComponent,
+    ProductComponent,
+    ProductInfoComponent
   ],
   imports: [
     BrowserModule,
