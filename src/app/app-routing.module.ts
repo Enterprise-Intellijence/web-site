fd9d07cfd029d2ec4c7e3486629e48083fcc27b9
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-07 19:04:21 +0200

    navbar progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,20 @@ import { ProfileDetailsComponent } from './pages/settings/profile-details/profil
 import { AccountSettingsComponent } from './pages/settings/account-settings/account-settings.component';
 import { NotificationsComponent } from './pages/settings/notifications/notifications.component';
 import { PaymentsComponent } from './pages/settings/payments/payments.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  // { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   // { path: 'user/:id', component: ProfilePageComponent },
+  // { path: 'products/new', component: NewProductPageComponent },
+  // { path: 'products/:id', component: ProductPageComponent },
   // { path: 'search', component: SearchPageComponent },
   // { path: 'login', component: LoginPageComponent },
   // { path: 'register', component: RegisterPageComponent },
   {
     path: 'settings', children: [
+      { path: '', redirectTo: 'profile', pathMatch: 'full'},
       { path: 'profile', component: ProfileDetailsComponent },
       { path: 'account', component: AccountSettingsComponent },
       { path: 'notifications', component: NotificationsComponent },
@@ -22,8 +26,7 @@ const routes: Routes = [
       { path: 'payments', component: PaymentsComponent },
     ]
   },
-
-
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faHeart, faRightFromBracket, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'nav-bar',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  search_icon = faSearch;
+  loginIcon = faRightFromBracket;
+  messages_icon = faEnvelope;
+  likes_icon = faHeart;
+  user_icon = faUser;
 
   isCollapsed = true;
+  isLoggedIn = true;
 }
