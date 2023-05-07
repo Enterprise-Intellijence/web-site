938f823095d212fb0bb9383377ded87eac75f16a
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-07 16:19:52 +0200

    removed dumb test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DumbTestComponent } from './components/dumb-test/dumb-test.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProfileDetailsComponent } from './pages/settings/profile-details/profile-details.component';
@@ -18,7 +17,6 @@ import { NotificationsComponent } from './pages/settings/notifications/notificat
 @NgModule({
   declarations: [
     AppComponent,
-    DumbTestComponent,
     NavBarComponent,
     ProfileDetailsComponent,
     SettingsComponent,
