95cbc2a1a52f226a4ad6b65fe3a5b46ac1ff475f
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-31 17:56:05 +0200

    working on messages page

---
+++ src/app/pages/messages-page/messages-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './messages-page.component.html',
+  styleUrls: ['./messages-page.component.scss']
+})
+export class MessagesPageComponent {
+
+}
