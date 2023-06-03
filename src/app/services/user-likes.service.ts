e6d700459016052fd31158923c800758f5cf9f27
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-03 20:09:43 +0200

    user likes and other stuff

--- src/app/services/user-likes.service.ts
+++ src/app/services/user-likes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ProductDTO } from './api-service';
+import { ProductBasicDTO, UserControllerService } from './api-service';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -8,28 +8,47 @@ import { BehaviorSubject } from 'rxjs';
 export class UserLikesService {
 
 
-  LikedProductsSet = new Set<ProductDTO>();
+  LikedProductsSet = new Set<ProductBasicDTO>();
 
-  LikedProducts$ = new BehaviorSubject<ProductDTO[]>([]);
+  LikedProducts$ = new BehaviorSubject<ProductBasicDTO[]>([]);
   LikedProductsCount$ = new BehaviorSubject<number>(0);
 
-
-
-
-  constructor() { }
-
-
-  public likeProduct(product: ProductDTO) {
-    this.LikedProductsSet.add(product);
-    this.updateLikedProducts();
+  constructor(private userService: UserControllerService) {
+    this.getAllLikedProducts();
   }
 
-  public unlikeProduct(product: ProductDTO) {
-    this.LikedProductsSet.delete(product);
-    this.updateLikedProducts();
+
+  public likeProduct(product: ProductBasicDTO) {
+    if (product.id == undefined)
+      throw new Error("Product id is undefined");
+
+    this.userService.like(product.id).subscribe({
+      next: () => {
+        this.LikedProductsSet.add(product);
+        this.updateLikedProducts();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
-  public toggleLikeProduct(product: ProductDTO) {
+  public unlikeProduct(product: ProductBasicDTO) {
+    if (product.id == undefined)
+      throw new Error("Product id is undefined");
+
+    this.userService.unlike(product.id).subscribe({
+      next: () => {
+        this.LikedProductsSet.delete(product);
+        this.updateLikedProducts();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  public toggleLikeProduct(product: ProductBasicDTO) {
     if (this.isProductLiked(product)) {
       this.unlikeProduct(product);
     }
@@ -38,13 +57,41 @@ export class UserLikesService {
     }
   }
 
-  public isProductLiked(product: ProductDTO) {
+  public isProductLiked(product: ProductBasicDTO) {
     return this.LikedProductsSet.has(product);
   }
 
   updateLikedProducts() {
     this.LikedProducts$.next(Array.from(this.LikedProductsSet));
     this.LikedProductsCount$.next(this.LikedProductsSet.size);
+  }
+
+  private getAllLikedProducts() {
+    this.LikedProductsSet.clear();
+    this.getNextLikedProductsPage(0);
+  }
+
+  private getNextLikedProductsPage(page: number) {
+    const PAGE_SIZE = 50;
+
+    this.userService.getLikedProducts(page, PAGE_SIZE).subscribe({
+      next: (productsPage) => {
+        if (productsPage.content != undefined) {
+          productsPage.content.forEach(product => {
+            this.LikedProductsSet.add(product);
+          });
+        }
+
+        if (productsPage.last == true) {
+          this.updateLikedProducts();
+        }
+        else
+          this.getNextLikedProductsPage(page + 1);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
   }
 
 
