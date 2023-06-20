17f7d728087bf2d23f28639c5df1617d49a0e863
Emanuele Conforti <manu.conforti@gmail.com>
2023-06-20 11:41:30 +0200

    new product page modified

--- src/app/pages/new-product-page/new-product-page.component.ts
+++ src/app/pages/new-product-page/new-product-page.component.ts
@@ -1,33 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { ProductDTO } from 'src/app/services/api-service';
+import { ProductCategoriesService } from 'src/app/services/product-categories.service';
+import { ProductCategory } from 'src/app/models/product-category';
+import { ProductControllerService } from 'src/app/services/api-service';
 
 @Component({
   selector: 'new-product-page',
   templateUrl: './new-product-page.component.html',
   styleUrls: ['./new-product-page.component.scss']
 })
-export class NewProductPageComponent {
+export class NewProductPageComponent implements OnInit {
 
-  imagesLoaded: Array<SafeUrl> = new Array<SafeUrl>();
-  title: string = "";
-  description: string = "";
-  price: string = "";
-  selectedCategory = "";
-  selectedParentCategory = "";
-  selectedChildCategory = "";
+  imagesLoaded: Array<File> = new Array<File>();
+  filesLoaded: Array<File> = new Array<File>();
+  primaryCategories: Array<ProductCategory> = new Array<ProductCategory>();
+  secondaryCategories: Array<ProductCategory> = new Array<ProductCategory>();
+  tertiaryCategories: Array<ProductCategory> = new Array<ProductCategory>();
 
-  printSizeImgs() {
-    console.log("S: " + this.imagesLoaded.length);
+  selectedCategory?: string;
+  selectedSecondaryCategory?: string;
+  selectedTertiaryCategory?: string;
+  title?: string;
+  description?: string;
+  price?: string;
+
+
+
+  processFile(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event: any) => {
+      this.filesLoaded.push(file);
+      this.imagesLoaded.push(event.target.result);
+      console.log("file: ", file);
+      console.log("img: ", event.target.result);
+    });
+
+    reader.readAsDataURL(file);
   }
 
-  onFileSelected(event: any) {
-    if(event.target.files) {
-        this.imagesLoaded.push(this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(event.target.files[0])));
-    }
+  onCategorySelected(i: number) {
+    console.log("selected cat: ", i);
+    this.secondaryCategories = this.primaryCategories[i].childCategories;
   }
 
-  constructor(private sanitizer: DomSanitizer) {
-    this.categoryList = Object.keys(ProductDTO.ProductCategoryEnum);
+  onSecondaryCategorySelected(i: number) {
+    console.log("selected cat: ", i);
+    this.tertiaryCategories = this.secondaryCategories[i].childCategories;
   }
+
+  uploadProduct() {
+    console.log("product uploaded");
+  }
+
+  ngOnInit(): void {
+    this.categoriesService.onCategoriesLoaded.subscribe(() => {
+      this.primaryCategories = this.categoriesService.primaryCategories;
+    });
+  }
+
+  constructor(
+    private categoriesService: ProductCategoriesService,
+    private productService: ProductControllerService) {
+  }
+  
 }
