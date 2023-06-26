33b42893ea16d3ad716e2f1165eaf2dd3a3d7288
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-06-26 09:56:19 +0200

    Added review component to profile

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductBasicDTO, ReviewControllerService, ReviewDTO, UserDTO } from 'src/app/services/api-service';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'review',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent {
+
+  user: UserDTO | null = null;
+  reviewPageMap: Map<number, Array<ReviewDTO>> = new Map<number, Array<ReviewDTO>>();
+  pageNumber: number = 0;
+  pageSize: number = 10;
+  totalElements: number = 0;
+  totalPages: number = 0;
+
+  constructor(private currentUserService: CurrentUserService,
+              private reviewService: ReviewControllerService) { }
+
+  ngOnInit(): void {
+    this.currentUserService.user$.subscribe(user => {
+      this.user = user;
+      this.loadReviews();
+    })
+  }
+
+  loadReviews() {
+
+    let userId = this.user?.id;
+
+    this.reviewService.allReviewReceived(this.pageNumber - 1, this.pageSize).subscribe(page => {
+      this.reviewPageMap.set(this.pageNumber, page.content!);
+      this.totalPages = page.totalPages!;
+      this.totalElements = page.totalElements!;
+      console.log(page);
+
+    });
+  }
+
+  changePage() {
+    this.loadReviews();
+  }
 
 }
