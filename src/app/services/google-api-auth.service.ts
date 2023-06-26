36675faefce4f572f66743b5f61a8a0f76b54b6a
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-26 19:17:02 +0200

    run npm install ng-gapi and created google api service

---
+++ src/app/services/google-api-auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { GoogleAuthService } from 'gapi';
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleApiAuthService {
+
+  constructor() {
+
+
+  }
+}
