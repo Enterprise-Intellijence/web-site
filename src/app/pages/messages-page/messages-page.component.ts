9096b551487581b68e444981fd2e7076d3058e4e
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-28 21:22:48 +0200

    work on message page

--- src/app/pages/messages-page/messages-page.component.ts
+++ src/app/pages/messages-page/messages-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { faCircleInfo, faInfo, faInfoCircle, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { ConversationDTO, MessageDTO } from 'src/app/services/api-service';
 import { ChatService } from 'src/app/services/chat.service';
@@ -7,7 +8,7 @@ import { ChatService } from 'src/app/services/chat.service';
   templateUrl: './messages-page.component.html',
   styleUrls: ['./messages-page.component.scss']
 })
-export class MessagesPageComponent {
+export class MessagesPageComponent implements OnInit {
   writeIcon = faPenToSquare;
   infoIcon = faCircleInfo;
 
@@ -17,10 +18,14 @@ export class MessagesPageComponent {
   messages: MessageDTO[] = [];
 
 
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private activatedRoute: ActivatedRoute) {
     this.chatService.conversations$.subscribe(conversations => {
       this.conversations = conversations;
+      this.selectedConversation = this.conversations[0];
     });
+  }
+  ngOnInit(): void {
+
   }
 
 }
