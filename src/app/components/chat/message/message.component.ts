a7f8e1c2831c0577f70f970b00cf33f76f86d311
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-29 01:10:17 +0200

    more chat work

--- src/app/components/chat/message/message.component.ts
+++ src/app/components/chat/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { ConversationDTO, MessageDTO } from 'src/app/services/api-service';
 import { CurrentUserService } from 'src/app/services/current-user.service';
 
@@ -9,13 +10,16 @@ import { CurrentUserService } from 'src/app/services/current-user.service';
 })
 export class MessageComponent {
 
+  faCheck = faCheck;
 
   @Input() message!: MessageDTO;
   @Input() conversation!: ConversationDTO;
 
+
   get otherUser() { return this.conversation.otherUser; }
   get myself() { return this.currentUserService.user; }
-
+  get isRead() { return this.message.messageStatus == 'READ'; }
+  get isOffer() { return this.message.offer != null; }
 
   constructor(private currentUserService: CurrentUserService) { }
 
