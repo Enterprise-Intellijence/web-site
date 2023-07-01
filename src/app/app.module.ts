0838cff1bc737668dfbc54ee8580641b6e3c9951
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-01 11:26:52 +0200

    changed backend api base path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,6 +62,7 @@ import { EmptyClosetComponent } from './components/empty-closet/empty-closet.com
 import { ClickStopPropagationDirective } from "./directives/click-stop-propagation.directive"
 import { AuthGuard } from "./guards/auth.guard"
 import { AdminGuard } from "./guards/admin.guard"
+import { Config } from './models/config';
 
 
 const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
@@ -155,7 +156,7 @@ export const API_INTERCEPTOR_PROVIDER: Provider = {
     DropzoneModule,
     BackendApiModule.forRoot(() => new BackendApiConfig({
       withCredentials: false,
-      basePath: 'https://localhost:8443'
+      basePath: Config.basePath,
     })),
     CountryCityModule.forRoot(() => new CountryCiyConfig({
       apiKeys: {"X-CSCAPI-KEY": "ZXJ6emFVYUxmWnhxN2YzZnpQWFY3TENnRmF2SUd6cHZxTWdvRGdKWQ=="}
--- src/app/models/config.ts
+++ src/app/models/config.ts
@@ -1,7 +1,8 @@
 
 export class Config {
-    
-    static readonly basePath: string = "https://localhost:8443/api/v1/";
 
-    static readonly basePathCountryCityAPI: string = 'https://api.countrystatecity.in/v1'; 
+    static readonly basePath: string = "http://localhost:8080";
+    static readonly apiBasePath: string = "http://localhost:8080/api/v1";
+
+    static readonly basePathCountryCityAPI: string = 'https://api.countrystatecity.in/v1';
 }
