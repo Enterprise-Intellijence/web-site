b18c3ab7767aebfd692f1a1e9ae50ddfe049eddf
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-02 15:52:19 +0200

    working on offer message

--- src/app/components/chat/message/message.component.ts
+++ src/app/components/chat/message/message.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { ConversationDTO, MessageDTO } from 'src/app/services/api-service';
+import { Component, Input, OnInit } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faCheck, faCross, faHourglassHalf, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { ConversationDTO, MessageDTO, OfferControllerService, OfferDTO } from 'src/app/services/api-service';
 import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
@@ -8,22 +9,85 @@ import { CurrentUserService } from 'src/app/services/current-user.service';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss']
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
+
 
   faCheck = faCheck;
+  faXMark = faXmark;
+  faHourglassHalf = faHourglassHalf;
+
+
+
+  offerStatusIcon: IconProp = this.faCheck;
+  offerStatusText: string = 'Offer accepted';
+  offerStatusClass: string = 'bg-success';
+
 
   @Input() message!: MessageDTO;
   @Input() conversation!: ConversationDTO;
 
+  get product() { return this.message.product; }
+  get offer() { return this.message.offer; }
 
   get otherUser() { return this.conversation.otherUser; }
   get myself() { return this.currentUserService.user; }
   get isRead() { return this.message.messageStatus == 'READ'; }
-  get isOffer() { return this.message.offer != null; }
-
-  constructor(private currentUserService: CurrentUserService) { }
+  get containsOffer() { return this.message.offer != null; }
 
   public get isOwnMessage() { return (this.message.sendUser.id === this.myself?.id) ?? true; }
+
+
+  constructor(private currentUserService: CurrentUserService,
+    private offerService: OfferControllerService) { }
+
+  ngOnInit(): void {
+    this.UpdateOfferVisual();
+  }
+
+
+  private UpdateOfferVisual() {
+    if (this.containsOffer) {
+      if (this.message.offer?.state == 'ACCEPTED') {
+        this.offerStatusIcon = this.faCheck;
+        this.offerStatusText = 'Offer accepted';
+        this.offerStatusClass = 'bg-success';
+      }
+      else if (this.message.offer?.state == 'PENDING') {
+        this.offerStatusIcon = this.faHourglassHalf;
+        this.offerStatusText = 'Offer pending';
+        this.offerStatusClass = 'bg-warning';
+      }
+      else if (this.message.offer?.state == 'REJECTED') {
+        this.offerStatusIcon = this.faXMark;
+        this.offerStatusText = 'Offer rejected';
+        this.offerStatusClass = 'bg-danger';
+      }
+    }
+  }
+
+  declineOffer() {
+    if (!this.offer)
+      return;
+    var offer: OfferDTO = { ...this.offer, state: 'REJECTED' };
+
+    this.offerService.updateOffer(offer, offer.id!).subscribe((offer) => {
+      this.message.offer = offer;
+      this.UpdateOfferVisual();
+    });
+  }
+
+
+  acceptOffer() {
+    if (!this.offer)
+      return;
+    var offer: OfferDTO = { ...this.offer, state: 'ACCEPTED' };
+
+    this.offerService.updateOffer(offer, offer.id!).subscribe((offer) => {
+      this.message.offer = offer;
+      this.UpdateOfferVisual();
+    });
+  }
+
 
 
 
--- src/app/components/product-price/product-price.component.ts
+++ src/app/components/product-price/product-price.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnChanges } from '@angular/core';
-import { ProductDTO, ProductBasicDTO, CustomMoneyDTO, UserBasicDTO, UserDTO } from 'src/app/services/api-service';
+import { ProductDTO, ProductBasicDTO, CustomMoneyDTO, UserBasicDTO, UserDTO, OfferControllerService, OfferCreateDTO } from 'src/app/services/api-service';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faCartShopping, faCircleInfo, faCommentDollar, faEdit, faHeart as faHeartFull, faShareNodes } from '@fortawesome/free-solid-svg-icons';
 import { UserLikesService } from 'src/app/services/user-likes.service';
 import { Router } from '@angular/router';
 import { CurrentUserService } from 'src/app/services/current-user.service';
+import { NgForm } from '@angular/forms';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'product-price',
@@ -13,21 +15,57 @@ import { CurrentUserService } from 'src/app/services/current-user.service';
 })
 export class ProductPriceComponent implements OnChanges {
 
+
   isFav: boolean = false;
 
   notFavButtonText: string = "Add to favourites";
   isFavButtonText: string = "Remove from favourites";
 
+  get isSeller(): boolean {
+    return this.currentUser?.id === this.productDTO?.seller?.id;
+  }
+
   @Input() productDTO?: ProductDTO;
   priceWithProtection?: string;
+
   currentUser: UserDTO | null = null;
-  fullHeartIcon= faHeartFull;
-  emptyHeartIcon= faHeart;
-  faCartShopping= faCartShopping;
-  faShareNodes= faShareNodes;
-  faCircleInfo= faCircleInfo;
-  faCommentDollar= faCommentDollar;
-  faEdit= faEdit;
+
+
+  fullHeartIcon = faHeartFull;
+  emptyHeartIcon = faHeart;
+  faCartShopping = faCartShopping;
+  faShareNodes = faShareNodes;
+  faCircleInfo = faCircleInfo;
+  faCommentDollar = faCommentDollar;
+  faEdit = faEdit;
+
+
+  isMakingOffer: boolean = false;
+  offerAmount: number = this.productDTO?.productCost?.price || 0;
+  offerCurrency: CustomMoneyDTO.CurrencyEnum = this.productDTO?.productCost?.currency || "EUR";
+
+
+  constructor(
+    private currentUserService: CurrentUserService,
+    private offerService: OfferControllerService,
+    private userLikesService: UserLikesService,
+     private router: Router) {
+    this.userLikesService.LikedProducts$.subscribe((products) => {
+      this.isFav = userLikesService.isProductLikedById(this.productDTO?.id!);
+    });
+
+    this.currentUserService.user$.subscribe((user) => {
+      this.currentUser = user;
+    })
+  }
+
+  offerChanged($event: number) {
+    this.offerAmount = $event;
+    if (this.offerAmount < 0) {
+      this.offerAmount = 0;
+    }
+    console.log("offer changed: ", this.offerAmount);
+  }
 
 
   clickFavButton() {
@@ -41,7 +79,29 @@ export class ProductPriceComponent implements OnChanges {
   }
 
   clickOfferButton() {
+    this.isMakingOffer = !this.isMakingOffer;
   }
+
+  submitOffer(form: NgForm) {
+    if (form.valid) {
+      console.log("form valid");
+
+      var offer: OfferCreateDTO = {
+        amount: {
+          price: this.offerAmount,
+          currency: this.offerCurrency
+        },
+        product: this.productDTO,
+      }
+
+      this.offerService.createOffer(offer, "body").subscribe((offer) => {
+        console.log("offer created: ", offer);
+        this.isMakingOffer = false;
+      })
+
+    }
+  }
+
 
   clickInfoButton() {
     this.router.navigate(["/messages/new/", this.productDTO?.seller?.id], {
@@ -52,26 +112,21 @@ export class ProductPriceComponent implements OnChanges {
   }
 
   clickShareProduct() {
-
+    alert("coming soon!");
+    throw new Error('Method not implemented.');
   }
 
   clickEdit() {
-
+    alert("coming soon!");
+    throw new Error('Method not implemented.');
   }
 
   ngOnChanges() {
     this.isFav = this.userLikesService.isProductLikedById(this.productDTO?.id!);
+    this.offerAmount = this.productDTO?.productCost?.price || 0;
+    this.offerCurrency = this.productDTO?.productCost?.currency || "EUR";
   }
 
-  constructor(private currentUserService: CurrentUserService ,private userLikesService: UserLikesService, private router: Router) {
-    userLikesService.LikedProducts$.subscribe((products) => {
-      this.isFav = userLikesService.isProductLikedById(this.productDTO?.id!);
-      console.log(this.isFav)
-    });
 
-    currentUserService.user$.subscribe( (user) => {
-      this.currentUser = user;
-    })
-  }
 
 }
