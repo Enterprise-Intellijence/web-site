ff0a6d6539282bc88976e4694888bbcdb222f34f
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-02 22:57:25 +0200

    closet now loads private products too, and filtered product improved

--- src/app/components/closet/closet.component.ts
+++ src/app/components/closet/closet.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { Observable } from 'rxjs';
 import { FilterOptions } from 'src/app/models/filter-options';
-import { UserBasicDTO } from 'src/app/services/api-service';
+import { PageProductBasicDTO, ProductControllerService, UserBasicDTO } from 'src/app/services/api-service';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'closet',
@@ -10,18 +12,33 @@ import { UserBasicDTO } from 'src/app/services/api-service';
 export class ClosetComponent implements OnChanges {
 
   @Input() visitedUser: UserBasicDTO | null = null;
+
+  customLoadMethod?: (page: number, pageSize: number) => Observable<PageProductBasicDTO>;
+
+  get isCurrentUser() {
+    return this.visitedUser?.id == this.currentUser.user?.id;
+  }
+
   filter!: FilterOptions;
   isProductEmpty!: boolean;
 
-  constructor() { }
+  constructor(private currentUser: CurrentUserService, private productController: ProductControllerService) { }
 
   ngOnChanges(): void {
     this.filter = new FilterOptions();
     this.filter.userId = this.visitedUser?.id;
+
+    if(this.isCurrentUser){
+      this.customLoadMethod = this.productController.getMyProducts.bind(this.productController);
+    }
+
     console.log(this.isProductEmpty);
   }
 
   setIsProductEmpty(isProductEmpty: boolean) {
     this.isProductEmpty = isProductEmpty;
   }
+
+
+
 }
