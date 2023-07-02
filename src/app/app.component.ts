4c78480ebf16d6cda8b7b98ae3d3bc61031f8a13
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-02 12:42:42 +0200

    more consistent login with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CredentialResponse, PromptMomentNotification } from 'google-one-tap';
 import { NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiAuthService } from './services/api-auth.service';
+import { GoogleApiAuthService } from './services/google-api-auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,62 +12,26 @@ import { ApiAuthService } from './services/api-auth.service';
 export class AppComponent implements OnInit {
   title = 'enterprise-intellijence';
 
-  constructor(private ngZone: NgZone, private router: Router, private apiAuth: ApiAuthService) {}
+
+
+  constructor(private ngZone: NgZone, private router: Router, private apiAuth: ApiAuthService, private googleApiAuth: GoogleApiAuthService) { }
 
   ngOnInit(): void {
-    if(!this.apiAuth.isLoggedIn()){
-      this.initializeGoogleAuth();
-    }
-    else{
-      console.log("User is logged in, no need for google auth");
-    }
-  }
+    this.apiAuth.isLoggedIn$.subscribe((isLoggedIn) => {
+      if (!isLoggedIn && this.googleApiAuth.isGoogleLibraryLoaded$.value) {
+        this.googleApiAuth.prompt();
+      }
+    });
 
-
-  private initializeGoogleAuth() {
     // @ts-ignore
     window.onGoogleLibraryLoad = () => {
       console.log('Google\'s One-tap sign in script loaded!');
+      this.googleApiAuth.isGoogleLibraryLoaded$.next(true);
+      this.googleApiAuth.initialize();
 
-      // @ts-ignore
-      google.accounts.id.initialize({
-        // Ref: https://developers.google.com/identity/gsi/web/reference/js-reference#IdConfiguration
-        client_id: '1049827257011-ete5d6u7kjq601i6ra2ukaus5nsd3het.apps.googleusercontent.com',
-        callback: this.handleCredentialResponse.bind(this),
-        auto_select: true,
-        cancel_on_tap_outside: false
-      });
-
-      // OPTIONAL: In my case I want to redirect the user to an specific path.
-      // @ts-ignore
-      google.accounts.id.prompt((notification: PromptMomentNotification) => {
-        console.log('Google prompt event triggered...');
-
-        if (notification.getDismissedReason() === 'credential_returned') {
-          this.ngZone.run(() => {
-            this.router.navigate(['/'], { replaceUrl: true });
-            console.log('Welcome back!');
-          });
-        }
-      });
+      if(!this.apiAuth.isLoggedIn()) {
+        this.googleApiAuth.prompt();
+      }
     };
   }
-
-  handleCredentialResponse(response: CredentialResponse) {
-    // Decoding  JWT token...
-    this.ngZone.run(() => {
-      this.apiAuth.googleLogin(response.credential).subscribe();
-    });
-
-    let decodedToken: any | null = null;
-    try {
-      decodedToken = JSON.parse(atob(response?.credential.split('.')[1]));
-    } catch (e) {
-      console.error('Error while trying to decode token', e);
-    }
-    console.log('decodedToken', decodedToken);
-  }
-
-
-
 }
--- src/app/services/google-api-auth.service.ts
+++ src/app/services/google-api-auth.service.ts
@@ -1,11 +1,80 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { CredentialResponse, PromptMomentNotification } from 'google-one-tap';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ApiAuthService } from './api-auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleApiAuthService {
 
-  constructor() {
 
+  isGoogleLibraryLoaded$ = new BehaviorSubject<boolean>(false);
+  isGoogleInitialized$ = new BehaviorSubject<boolean>(false);
+
+
+
+  constructor(
+    private apiAuth: ApiAuthService,
+    private ngZone: NgZone,
+    private router: Router,
+  ) { }
+
+
+  public initialize() {
+    // @ts-ignore
+    console.log('Initializing Google\'s One-tap sign in script...');
+
+    google.accounts.id.initialize({
+      // Ref: https://developers.google.com/identity/gsi/web/reference/js-reference#IdConfiguration
+      client_id: '1049827257011-ete5d6u7kjq601i6ra2ukaus5nsd3het.apps.googleusercontent.com',
+      callback: (credential) => { this.responseCallback(credential) },
+      auto_select: true,
+      cancel_on_tap_outside: false,
+      log_level: 'warn',
+    });
+
+    this.isGoogleInitialized$.next(true);
+  }
+
+
+  responseCallback(response: CredentialResponse) {
+    console.log('initializeCallback', response);
+
+    this.ngZone.run(() => {
+      this.apiAuth.googleLogin(response.credential).subscribe(
+        (data) => {
+          console.log('Google login successful!', data);
+          this.router.navigate(['/'], { replaceUrl: true });
+        }
+      );
+    });
+
+    // Decoding  JWT token...
+    let decodedToken: any | null = null;
+    try {
+      decodedToken = JSON.parse(atob(response?.credential.split('.')[1]));
+      console.log('decodedToken', decodedToken);
+    } catch (e) {
+      console.error('Error while trying to decode token', e);
+    }
+  }
+
+
+  prompt() {
+
+    google.accounts.id.prompt((notification: PromptMomentNotification) => {
+      console.log('Google prompt event triggered...');
+
+      // // OPTIONAL: In my case I want to redirect the user to an specific path.
+      // if (notification.getDismissedReason() === 'credential_returned') {
+      //   this.ngZone.run(() => {
+      //     this.router.navigate(['/'], { replaceUrl: true });
+      //     console.log('Welcome back!');
+      //   });
+      // }
+
+    });
   }
 }
