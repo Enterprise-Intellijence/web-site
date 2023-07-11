3590115ee5398a460d0055dc44843a885dd7cb60
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-11 18:06:25 +0200

    order card progress

--- src/app/components/orders/order-card/order-card.component.ts
+++ src/app/components/orders/order-card/order-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpRightFromSquare, faCheck, faStar } from '@fortawesome/free-solid-svg-icons';
 import { OrderBasicDTO } from 'src/app/services/api-service';
 
 @Component({
@@ -8,6 +8,8 @@ import { OrderBasicDTO } from 'src/app/services/api-service';
   styleUrls: ['./order-card.component.scss']
 })
 export class OrderCardComponent {
+  faStar = faStar;
+  faCheck = faCheck;
 
   @Input("orderBasic") order!: OrderBasicDTO;
   faOpen = faArrowUpRightFromSquare;
@@ -33,5 +35,26 @@ export class OrderCardComponent {
         throw new Error(`Unknown state: ${this.state}`);
     }
     return prefix + val + suffix;
+  }
+
+  getStateIcon() {
+    switch (this.state) {
+      case 'CANCELED':
+        return ;
+      case 'PENDING':
+        return ;
+      case 'PURCHASED':
+        return ;
+      case 'SHIPPED':
+        return ;
+      case 'DELIVERED':
+        return ;
+      case 'COMPLETED':
+        return ;
+      case 'REVIEWED':
+        return faStar;
+      default:
+        throw new Error(`Unknown state: ${this.state}`);
+    }
   }
 }
