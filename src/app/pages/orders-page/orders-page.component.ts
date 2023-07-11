dc35e060c52f2c633fe7a2032b91b1d92e4089de
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-11 15:56:11 +0200

    starting to work on orders page

--- src/app/pages/orders-page/orders-page.component.ts
+++ src/app/pages/orders-page/orders-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { OrderControllerService, OrderDTO } from 'src/app/services/api-service';
 
 @Component({
   selector: 'orders-page',
   templateUrl: './orders-page.component.html',
   styleUrls: ['./orders-page.component.scss']
 })
-export class OrdersPageComponent {
+export class OrdersPageComponent implements OnInit {
+
+  faOpen = faArrowUpRightFromSquare;
+
+  pageSize: number = 5;
+  visualPage: number = 1;
+  totalOrdersCount: number = 0;
+
+  orders: OrderDTO[] = [];
+
+  constructor(private orderService: OrderControllerService) { }
+
+
+  ngOnInit(): void {
+    this.loadProductsPage();
+  }
+
+  loadProductsPage() {
+    this.orderService.getAllOrdersOfUser(this.visualPage-1, this.pageSize).subscribe((orders) => {
+      this.totalOrdersCount = orders.totalElements || 0;
+      this.orders = orders.content || [];
+    });
+  }
+
 
 }
